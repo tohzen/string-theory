3ee89747a5d1fb29029042197d75622f61723f28
Tohzen <melvin.l.mobley@gmail.com>
2021-03-04 23:04:29 -0500

    finished

--- main.js
+++ main.js
@@ -2,9 +2,212 @@
  * YOUR CODE HERE *
  ******************/
 
+ function xify (string){
+  let str = '';
+  for (const letter of string){
+    str= str + 'x';
+  }
+  return str;
+}
+
+function yellingChars(str){
+  let newStr = "";
+  for (const letter of str){
+    newStr = newStr + letter +'!';
+  }
+  return newStr;
+}
+
+function indexedChars(str){
+  let newStr = '';
+  let i=0;
+  while (i < str.length){
+    newStr = newStr + i.toString() + str[i];
+    i++;
+  }
+  return newStr;
+}
+
+function numberedChars (str){
+  let newStr = '';
+  let i=0;
+  while (i < str.length){
+    newStr = newStr + "("+ (i+1).toString() + ")" + str[i];
+    i++
+  }
+  return newStr;
+}
+
+function exclaim (str){
+  let newStr = "";
+  for (const letter of str){
+    if (letter === '?'){
+      newStr = newStr + '!';
+    } else if (letter === '.'){
+      newStr = newStr + '!'
+    } else{
+      newStr = newStr + letter;
+    }
+  }
+return newStr;
+}
+
+function repeatIt (str, n){
+  let newStr = "";
+  let i=0;
+  while (i<n){
+    newStr = newStr + str;
+    i++;
+  }
+return newStr
+}
+
+function truncate(str){
+  let newStr = '';
+  if (str.length<=18){
+    for (let i=0;i<str.length;i++){
+      newStr = newStr + str[i];
+    }
+  } else {
+      for (let i=0; i<15;i++){
+        newStr = newStr + str[i];
+      }
+      newStr = newStr + '...';
+  }
+  return newStr;
+}
+
+function ciEmailify(name){
+  let email = '';
+  for (let i=0;i<name.length;i++){
+    if (name[i] === ' '){
+      email = email+'.';
+    } else {
+      email = email+ name[i]
+    }
+  }
+  email = (email+'@codeimmersives.com').toLowerCase()
+  return email
+}
 
 
+function reverse (str) {
+  let reverseStr = ""
+  for (let i=str.length-1;i>=0;i--){
+    reverseStr = reverseStr+str[i];
+  }
+  return reverseStr;
+}
 
+function onlyVowels(str){
+  
+    let newStr = '';
+  for (const letter of str){
+    let vowel = (
+      letter ==='e' || 
+      letter ==='i' || 
+      letter ==='o' || 
+      letter ==='u' || 
+      letter ==='a' ||
+      letter ==='E' || 
+      letter ==='I' || 
+      letter ==='O' || 
+      letter ==='U' || 
+      letter ==='A'
+    );
+    if (vowel){
+      newStr = newStr + letter;
+    }
+  }
+return newStr;
+}
+
+function crazyCase(str){
+  let newStr = '';
+  for (let i = 0;i<str.length;i++){
+    if (i%2 === 0){
+      let letter = str[i].toLowerCase()
+      newStr = newStr + letter
+    } else {
+      let letter1 = str[i].toUpperCase()
+      newStr=newStr+letter1
+    }
+  }
+  return newStr;
+}
+
+function titleCase(str){
+  let newStr = '';
+  let rep = false;
+  for (let i=0; i<str.length; i++){
+    if (i===0){
+      rep = true
+    }
+    if (rep === true) {
+      rep = false;
+    } else if (rep === false){
+      newStr = newStr + str[i].toLowerCase();
+    }
+    if (str[i] === ' '){
+      newStr = newStr + str[i+1].toUpperCase();
+      rep = true;
+    } else if (i===0){
+      newStr = newStr + str[i].toUpperCase();
+    } 
+    
+  }
+  return newStr;
+  }
+
+
+function camelCase(str){
+  let newStr = ''
+  let rep = false;
+  for (let i = 0; i<str.length;i++){
+      if (rep === true) {
+        rep = false;
+      } else{
+    if (str[i] === ' '){
+      newStr = newStr + str[i+1].toUpperCase();
+      rep = true;
+    } else {
+      newStr = newStr + str[i].toLowerCase();
+    }
+  }
+}
+  return newStr;
+}
+
+function crazyCase2ReturnOfCrazyCase(str){
+      let newStr = '';
+      let space = 0;
+      for (let i = 0;i<str.length;i++){
+        if (space%2 === 0){
+          if (i%2 === 0){
+            let letter = str[i].toLowerCase()
+            newStr = newStr + letter
+          } else {
+            let letter1 = str[i].toUpperCase()
+            newStr=newStr+letter1
+          }
+          if (str[i] === ' '){
+            space++
+          }
+          } else if (space%2 === 1){
+          if ((i-1)%2 === 0){
+          let letter2 = str[i].toLowerCase()
+            newStr = newStr + letter2
+          } else {
+            let letter3 = str[i].toUpperCase()
+            newStr=newStr+letter3
+          }
+          if (str[i] === ' '){
+            space++
+          }
+        }
+      }
+      return newStr;
+}
 
 
 
